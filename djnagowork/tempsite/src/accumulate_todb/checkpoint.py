8c8de93ec5005f343bcd1ab4ff40e6b1eee60b8f
abhiagar90 <abhiagar90@gmail.com>
2015-07-09 01:38:23 +0530

    addding full code

--- djnagowork/tempsite/src/accumulate_todb/checkpoint.py
+++ djnagowork/tempsite/src/accumulate_todb/checkpoint.py
@@ -1,3 +1,243 @@
+from py2neo import Graph, Node, Relationship
+
+def getGraph(server,port,username,password):
+    con_url = 'http://'+username+':'+password+'@'+server+':'+port+'/db/data/'
+    return Graph(con_url)
+
+##helper function to help delete all junk created in graph
+## use only during development
+def delete_all(graph):
+    graph.delete_all()
+    
+##this is how we use the intenal ids of the graph
+##should we use it?? most people say no
+## anyways, made the method for future use
+## Use : getNodeByInternalId(graph, 152)
+def getNodeByInternalId(graph,id):
+    a  = graph.node(id) #getting by id given internally by neo4j
+    a.pull()
+    return a
+
+## similar to above
+## Use : getRelByInternalId(graph, 4) 
+def getRelByInternalId(graph,id):
+    a  = graph.relationship(id) 
+    a.pull()
+    return a
+
+##returns Relation which has custom id in 'ids' as we have given it
+## but the relation should be latest, that is cp_end should be infinity, otherwise throws an error
+#Use:
+#rc = getLatestRelById(graph, '4') ##IndexError if not latest!! TODO
+#print isinstance(rc,Relationship)
+#rc
+def getLatestRelById(graph,ids):
+    query = "match ()-[r {relid:'"+ids+"',cp_end:'Infinity'}]-() return r"
+    #print query
+    rc = graph.cypher.execute("match ()-[r {relid:'"+ids+"',cp_end:'Infinity'}]-() return r")
+    #print len(rc)
+    #print rc
+    return rc[0][0]
+
+##Use: id used is what we have given by ourselves
+##n1 = getLatestNodeById(graph, '1') # ##IndexError if not latest!! TODO
+#print isinstance(n1,Node)
+#n1
+def getLatestNodeById(graph,ids):
+    print '' + str(ids)
+    rc = graph.cypher.execute("match (n {nodeid:'"+ids+"',cp_end:'Infinity'}) return n")
+    return rc[0][0]
+
+##Use: getLastCheckPoint(graph)
+## returns a string telling which was the last checkpoint in the graph
+def getLastCheckPoint(graph): #returns a string
+    a=graph.find_one('version_cp')
+    return str(int(a.properties['next'])-1)
+
+##Use: getNextCheckPoint(graph)
+##returns a string telling which will be the next checkpoint in the graph
+def getNextCheckPoint(graph): ##returns a string
+    a=graph.find_one('version_cp')
+    return a.properties['next']
+
+##Use: getNextNodeId(graph)
+#returns node id for next node if any is created
+def getNextNodeId(graph):
+    a=graph.find_one('node_id')
+    return a.properties['next']
+
+##Use: getNextRelId(graph)
+#returns rel id for next rel if any is created
+def getNextRelId(graph):
+    a=graph.find_one('rel_id')
+    return a.properties['next']
+
+#Use: incrementCheckPoint(graph)
+#to be used whwn an update has been done
+def incrementCheckPoint(graph):
+    a=graph.find_one('version_cp')
+    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.push()
+    return a
+
+#Use: incrementNodeId(graph)
+#to be used when a node with previous id has been created just now
+def incrementNodeId(graph):
+    a=graph.find_one('node_id')
+    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.push()
+    return a
+
+#Use: incrementRelId(graph)
+#to be used when a node with previous id has been created just now
+def incrementRelId(graph):
+    a=graph.find_one('rel_id')
+    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.push()
+    return a
+
+#Use: delete_with_cp(graph,node1,node2,rel2,rel2)
+##alternative to graph.delete(entities)
+#will push any chnages that you have done! so beware that object should not be out of sync with server
+def delete_with_cp(graph,*entities):
+    cp=getNextCheckPoint(graph)
+    for entity in entities: #to handle the tuple problem
+        entity["cp_end"]=cp
+    graph.push(*entities) #the star was the catch here
+    incrementCheckPoint(graph)
+
+
+#Use: create_with_cp(graph,node1,node2,rel2,rel2)
+##alternative to graph.create(entities)
+def create_with_cp(graph,*entities):
+    cp=getNextCheckPoint(graph)
+    stmt = create_with_cp_helper(graph,cp,*entities)
+    incrementCheckPoint(graph)
+    return stmt
+
+#Use: This method creates the entities with the checkpoint given in cp variable
+#TODO: copied from core.py, make it easy
+def create_with_cp_helper(graph,cp,*entities):
+    from py2neo.cypher.create import CreateStatement
+    statement = CreateStatement(graph)
+    #print entities
+    for entity in entities: #removed handle the tuple problem
+        entity["cp_start"]=cp
+        entity["cp_end"]="Infinity"
+        if(isinstance(entity,Node)): ##TODO: afterwards, reduce database hits somehow
+            ids=getNextNodeId(graph)
+            entity["nodeid"]=ids
+            incrementNodeId(graph)
+        elif(isinstance(entity,Relationship)):
+            ids=getNextRelId(graph)
+            entity["relid"]=ids
+            incrementRelId(graph)
+        statement.create(entity)
+    return statement.execute()
+
+
+#Use: This is supposed (to be alternative to graph.push(entities)
+#Relationships done till now, nodes left
+def push_with_cp(graph,*entities):
+    cp=getNextCheckPoint(graph)
+    stmt = push_with_cp_helper(graph,cp,*entities)
+    incrementCheckPoint(graph)
+    return stmt
+
+
+##TODO: test it completely and reduce database hits when possible.
+##push only when actually pushing some changes to the nodes and relationships
+##or else it will push exact same thing to the graph replicated with checkpointing
+def push_with_cp_helper(graph,cp,*entities):
+    s=''
+    for entity in entities: #removed the tuple problem now
+        if(isinstance(entity,Relationship)):
+            ##WARNING: deleting these print statements is a prob with py2neo!!!
+            #print 'st:' + str(entity.start_node)+' --  en:' + str(entity.end_node)
+            st=entity.start_node
+            en=entity.end_node
+            st_uri=entity.start_node.uri
+            en_uri=entity.end_node.uri
+            props=entity.properties
+            typed=entity.type
+            entity.unbind()
+            old_rel = getLatestRelById(graph, props["relid"])
+            old_rel["cp_end"]=str(int(cp)-1)
+            old_rel.push()
+            st.bind(st_uri)
+            en.bind(en_uri)
+            new_rel=copyRelationship(graph,props,st,en,typed)
+            stmt = create_with_cp_helper(graph,cp,new_rel)
+            entity.bind(new_rel.uri)
+            s+=str(stmt)
+        elif(isinstance(entity,Node)):
+            print 'nodenodenode'
+            #find if a prev relation esxisted with NEXT
+           
+                
+            #first push the new node just behind our current node
+            entity["cp_start"] = cp #not psuhed yet
+            prev_uri= entity.uri
+            nodeid=entity["nodeid"]
+            entity.unbind()
+            
+            
+            latest = ''
+            old_node = getLatestNodeById(graph, entity["nodeid"])
+            for r in old_node.match_incoming(rel_type="NEXT"):
+                latest = r
+                break
+            
+            new_node = copyNode(old_node)
+            new_node["cp_end"]=str(int(cp)-1)
+            new_rel = Relationship(new_node,"NEXT",old_node)
+            stmt  = graph.create(new_node,new_rel)
+            entity.bind(prev_uri)
+            entity.push()
+            s+=str(stmt)
+            #now chnage pointers to make the new node scond last
+            if(latest!=''):
+                print 'here'
+                prev=latest.start_node
+                prev.pull()
+                new_node.pull()
+                rel=Relationship(prev,"NEXT",new_node)
+                graph.create(rel)
+                print rel.bound
+                graph.delete(latest)
+                
+    return s
+            
+    
+
+#Use : copyRelationship(properties_of_relation,start_node,end_node,type_of_relation), 
+#returns a Relation copy exactly of the Relation in argument
+#the returned relation is a copy of the props, dir, of the relation within same nodes needs to be pushed after this
+def copyRelationship(graph,props,st,en,typed):
+    #print 'insidecopy:'+str(st)
+    start = getLatestNodeById(graph,st["nodeid"])
+    end=getLatestNodeById(graph,en["nodeid"])
+    kind=typed
+    r2=Relationship(start,kind,end)
+    for x in props:
+        r2[x]=props[x]
+    return r2
+
+#Use : copyNode(instance of a node) 
+#returns a Node copy exactly of the Node in argument
+#the returned relation is a copy of the props,labels of the node,  
+#node will needs to be pushed after this
+def copyNode(node):
+    labels = node.labels
+    props = node.properties
+    naya=Node()
+    for x in props:
+        naya[x]=props[x]
+    for x in labels:
+        naya.labels.add(x)
+    return naya
+
+
 ##INITIAL TESTS
 def create_with_cpTest(graph):
     ##use case when multiple related nodes, to create a sub graph created
@@ -62,6 +302,8 @@ def push_with_cpTest3(graph):
     push_with_cp(graph,node)
     return stmt
 #push_with_cpTest(graph) #TODO: save the descrption to the metadata
+
+
 
 ##Disclaimer: Dont ever forget to pull the relations!! We fear even to test!
 ##Very basic tests to do
@@ -243,4 +485,4 @@ def main():
 ##Check method for serailizing and de-serailizing
 #
 #gt=getGraph('localhost','7474','neo4j','yoyo')aaa=Node()
-#main()
+main()
