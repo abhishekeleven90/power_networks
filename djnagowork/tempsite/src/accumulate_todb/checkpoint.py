0a0dc482741cfcf36f37498169e462be31f135a7
abhiagar90 <abhiagar90@gmail.com>
2015-07-14 15:27:25 +0530

    removing string checkpoints, revert functionality

--- djnagowork/tempsite/src/accumulate_todb/checkpoint.py
+++ djnagowork/tempsite/src/accumulate_todb/checkpoint.py
@@ -1,4 +1,10 @@
+
+# coding: utf-8
+
+
 from py2neo import Graph, Node, Relationship
+
+INFINITY = 9223372036854775807
 
 def getGraph(server,port,username,password):
     con_url = 'http://'+username+':'+password+'@'+server+':'+port+'/db/data/'
@@ -32,9 +38,9 @@ def getRelByInternalId(graph,id):
 #print isinstance(rc,Relationship)
 #rc
 def getLatestRelById(graph,ids):
-    query = "match ()-[r {relid:'"+ids+"',cp_end:'Infinity'}]-() return r"
+    query = "match ()-[r {relid:"+str(ids)+",cp_end:"+str(INFINITY)+"}]-() return r"
     #print query
-    rc = graph.cypher.execute("match ()-[r {relid:'"+ids+"',cp_end:'Infinity'}]-() return r")
+    rc = graph.cypher.execute(query)
     #print len(rc)
     #print rc
     return rc[0][0]
@@ -44,8 +50,7 @@ def getLatestRelById(graph,ids):
 #print isinstance(n1,Node)
 #n1
 def getLatestNodeById(graph,ids):
-    query = "match (n {nodeid:'"+ids+"',cp_end:'Infinity'}) return n"
-    print query
+    query = "match (n {nodeid:"+str(ids)+",cp_end:"+str(INFINITY)+"}) return n"
     rc = graph.cypher.execute(query)
     return rc[0][0]
 
@@ -53,7 +58,7 @@ def getLatestNodeById(graph,ids):
 ## returns a string telling which was the last checkpoint in the graph
 def getLastCheckPoint(graph): #returns a string
     a=graph.find_one('version_cp')
-    return str(int(a.properties['next'])-1)
+    return a.properties['next']-1
 
 ##Use: getNextCheckPoint(graph)
 ##returns a string telling which will be the next checkpoint in the graph
@@ -77,7 +82,7 @@ def getNextRelId(graph):
 #to be used whwn an update has been done
 def incrementCheckPoint(graph):
     a=graph.find_one('version_cp')
-    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.properties['next'] = a.properties['next']+1
     a.push()
     return a
 
@@ -85,7 +90,7 @@ def incrementCheckPoint(graph):
 #to be used when a node with previous id has been created just now
 def incrementNodeId(graph):
     a=graph.find_one('node_id')
-    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.properties['next'] = a.properties['next']+1
     a.push()
     return a
 
@@ -93,17 +98,21 @@ def incrementNodeId(graph):
 #to be used when a node with previous id has been created just now
 def incrementRelId(graph):
     a=graph.find_one('rel_id')
-    a.properties['next'] = str(int(a.properties['next'])+1)
+    a.properties['next'] = a.properties['next']+1
     a.push()
     return a
 
 #Use: delete_with_cp(graph,node1,node2,rel2,rel2)
 ##alternative to graph.delete(entities)
 #will push any chnages that you have done! so beware that object should not be out of sync with server
+#TODO MAJOR: delete is not that easy! if a current node is deleted and not its rels!!! then?
+#  Solution should be: first del all rels in *entities, 
+#  and then delete nodes -- if no live rels for these nodes outgoing incoming, only then else error 
 def delete_with_cp(graph,*entities):
-    cp=getNextCheckPoint(graph)
     for entity in entities: #to handle the tuple problem
-        entity["cp_end"]=cp
+        #patch to handle the delete issue
+        #if a node is deleted now, that means it existed till last checkpoint
+        entity["cp_end"]=getLastCheckPoint(graph)
     graph.push(*entities) #the star was the catch here
     incrementCheckPoint(graph)
 
@@ -124,7 +133,7 @@ def create_with_cp_helper(graph,cp,*entities):
     #print entities
     for entity in entities: #removed handle the tuple problem
         entity["cp_start"]=cp
-        entity["cp_end"]="Infinity"
+        entity["cp_end"]=INFINITY
         if(isinstance(entity,Node)): ##TODO: afterwards, reduce database hits somehow
             ids=getNextNodeId(graph)
             entity["nodeid"]=ids
@@ -163,15 +172,20 @@ def push_with_cp_helper(graph,cp,*entities):
             typed=entity.type
             entity.unbind()
             old_rel = getRelAtHead(graph, props["relid"]) ##TODO: Use the cp_check variable here!
-            old_rel["cp_end"]=str(int(cp)-1)
-            old_rel.push()
+            
+            #Rels that are alive do normal push
+            if(old_rel["cp_end"]==INFINITY): 
+                old_rel["cp_end"]=cp-1
+                old_rel.push()
+            ##else dead rels become live if this if is missing when reverting
+                
             st.bind(st_uri)
             en.bind(en_uri)
             new_rel=copyRelationship(graph,props,st,en,typed)
             
             ##adding patch for keeping the relid same 
             new_rel["cp_start"]=cp
-            new_rel["cp_end"]="Infinity"
+            new_rel["cp_end"]=INFINITY
             new_rel["relid"]=props["relid"]
             stmt = graph.create(new_rel)
             ##patch finished
@@ -179,26 +193,26 @@ def push_with_cp_helper(graph,cp,*entities):
             entity.bind(new_rel.uri)
             s+=str(stmt)
         elif(isinstance(entity,Node)):
-            print 'nodenodenode'
             #find if a prev relation esxisted with NEXT
            
                 
             #first push the new node just behind our current node
             entity["cp_start"] = cp #not psuhed yet
-            entity["cp_end"] = "Infinity"
+            entity["cp_end"] = INFINITY
             prev_uri= entity.uri
             nodeid=entity["nodeid"]
             entity.unbind()
             
             
             latest = ''
-            old_node = getNodeAtHead(graph, entity["nodeid"])
+            old_node = getNodeAtHead(graph, nodeid)
             for r in old_node.match_incoming(rel_type="NEXT"):
                 latest = r
                 break
             
             new_node = copyNode(old_node)
-            new_node["cp_end"]=str(int(cp)-1)
+            if(new_node["cp_end"]==INFINITY): #else if dead node: just copy as it is
+                new_node["cp_end"]=cp-1
             new_rel = Relationship(new_node,"NEXT",old_node)
             stmt  = graph.create(new_node,new_rel)
             entity.bind(prev_uri)
@@ -206,7 +220,6 @@ def push_with_cp_helper(graph,cp,*entities):
             s+=str(stmt)
             #now chnage pointers to make the new node scond last
             if(latest!=''):
-                print 'here'
                 prev=latest.start_node
                 prev.pull()
                 new_node.pull()
@@ -246,6 +259,8 @@ def copyNode(node):
         naya.labels.add(x)
     return naya
 
+
+
 ##INITIAL TESTS
 def create_with_cpTest(graph):
     ##use case when multiple related nodes, to create a sub graph created
@@ -263,7 +278,7 @@ def create_with_cpTest(graph):
 ##rel only
 def push_with_cpTest(graph):
     ##use case when multiple related nodes, to create a sub graph created
-    rel = getLatestRelById(graph,'1') #change accordingly
+    rel = getLatestRelById(graph,1) #change accordingly
     print rel.uri
     rel["since"]=1999
     stmt  = push_with_cp(graph,rel)
@@ -276,7 +291,7 @@ def push_with_cpTest(graph):
 
 ##node only
 def push_with_cpTest2(graph):
-    node = getLatestNodeById(graph,'1') #change accordingly
+    node = getLatestNodeById(graph,1) #change accordingly
     print node.uri
     node["bye"]="last"
     stmt  = push_with_cp(graph,node)
@@ -289,11 +304,11 @@ def push_with_cpTest2(graph):
 ##rel only
 def push_with_cpTest3(graph):
     ##use case when multiple related nodes, to create a sub graph created
-    rel = getLatestRelById(graph,'1') #change accordingly
+    rel = getLatestRelById(graph,1) #change accordingly
     print rel.uri
     rel["since"]=1999
     
-    node = getLatestNodeById(graph,'1') #change accordingly
+    node = getLatestNodeById(graph,1) #change accordingly
     print node.uri
     node["bye"]="last"
     
@@ -303,7 +318,7 @@ def push_with_cpTest3(graph):
     node.pull()
     
     
-    node=getLatestNodeById(graph,'1') #change accordingly
+    node=getLatestNodeById(graph,1) #change accordingly
     print node.uri
     node["hi"]="new"
     
@@ -465,66 +480,130 @@ def test_TwoNodesOneRelation(graph):
     ##EVEN BEFORE DELETING A RELATION, you will have to pull
     ##this we can fix : TODO in delete method
     
-    #employee.pull()
-    #delete_with_cp(graph,first,second,employee)
+    employee.pull()
+    delete_with_cp(graph,first,second,employee)
+    
+    third=Node("Person",name="Abhishek Agarwal")
+    create_with_cp(graph,third)
+
+def test_limitInt(graph):
+    node=Node("Random",prop=9223372036854775000)
+    create_with_cp(graph,node)
+
 
 
 ##returns Relation which has custom id in 'ids' as we have given it
 ##and at this cp
 def getRelByIdAndCheckPoint(graph,ids,cp):
     #TODO:Add check for cp in both these methods
-    query = "match ()-[r {relid:'"+ids+"'}]-() where r.cp_start <='"+cp+"' and r.cp_end >='"+cp+"' return r"
+    query = "match ()-[r {relid:"+str(ids)+"}]->() where r.cp_start <="+str(cp)+" and r.cp_end >="+str(cp)+" return r"
     #print query
     rc = graph.cypher.execute(query)
+    if(len(rc)==0): return None ##TODO: make same changes to other get methods, if necessary
     return rc[0][0]
 
 ##Use: id used is what we have given by ourselves
 #and at this cp
 def getNodeByIdAndCheckPoint(graph,ids,cp):
-    query = "match (n {nodeid:'"+ids+"'}) where n.cp_start <='"+cp+"' and n.cp_end >='"+cp+"' return n"
+    query = "match (n {nodeid:"+str(ids)+"}) where n.cp_start <="+str(cp)+" and n.cp_end >="+str(cp)+" return n"
     #print query
     rc = graph.cypher.execute(query)
+    if(len(rc)==0): return None
     return rc[0][0]
 
 def getNodeAtHead(graph,ids):
-    query = "match (n {nodeid:'"+ids+"'}) return n order by n.cp_end desc limit 1"
+    query = "match (n {nodeid:"+str(ids)+"}) return n order by n.cp_end desc limit 1"
     rc = graph.cypher.execute(query)
     return rc[0][0]
 
 def getRelAtHead(graph,ids):
-    query = "match ()-[r {relid:'"+ids+"'}]-() return r order by r.cp_end desc limit 1"
+    query = "match ()-[r {relid:"+str(ids)+"}]-() return r order by r.cp_end desc limit 1"
     rc = graph.cypher.execute(query)
     return rc[0][0]
     
+##revert the graph to the checkpoint 'cp'
+##gives a new cp to all reverted nodes and rels
 def revert(graph,cp):
+    
+    #nextcp will be used if old nodes and rels are created back, so new cp nextcp is given to them
     nextcp = getNextCheckPoint(graph)
-    nodequery = "match (n) where n.cp_start>'"+cp+"'  return distinct n.nodeid"
+    
+    #handling nodes first
+    
+    #store in noderc all nodes that've changed or born since said cp, these are the nodes we will work at 
+    #all changed nodes need to be reverted back
+    #all born nodes will become dead
+    #born nodes also include nodes which were dead and made alive by previous revert, so they will become dead too, but catch is? just dead?? yes just dead. 
+    #since born and zombie nodes were dead technically at that point of time
+    nodequery = "match (n) where n.cp_start>"+str(cp)+" or (n.cp_end <> "+str(INFINITY)+" and "+str(cp)+"<=n.cp_end) return distinct n.nodeid"
     noderc = graph.cypher.execute(nodequery)
+    
+    #variable to tell us if we have to increment the cp, that is if any nodes/rels have actually been reverted back
     increment = False
+    
     for r in noderc:
+        
+        #this query's response will differentiate between born and changed nodes
+        #if old is None, node dint exist at that cp, and is existing now, make it dead
+        #else existed then so revert now at any cost regardless of now dead or live, revert back
         old = getNodeByIdAndCheckPoint(graph,r['n.nodeid'],cp)
-        naya = getNodeAtHead(graph,str(r['n.nodeid'])) ##TODO: change to get headoflinkedlist for dead nodes
-        print naya
-        naya.properties.clear()
-        naya.labels.clear()
-        for x in old.properties:
-            naya[x]=old[x]
-        for x in old.labels:
-            naya.labels.add(x)
-        push_with_cp_helper(graph,nextcp,naya)
+        #get at the head of the node list
+        #will update this one
+        #can be a dead node or live node
+        naya = getNodeAtHead(graph,str(r['n.nodeid'])) 
+        if old==None: #dead node at that cp, but first check if live right now, then make it dead, else let it be
+            #same logic as in deleting #TODO: make a delete helper?
+            ##existed till the previous checkpoint, from nextcp it is dead
+            if (naya['cp_end']==INFINITY):
+                ##this will happen : if live now and was dead at that time, kill it, with current props!
+                ##why with current props? because state matters in this context not the props! think!! so this is good!
+                naya['cp_end']=nextcp-1
+                graph.push(naya)
+            else:
+                pass ##added to remove confusion
+        else: #node was alive at that cp
+            #copy all the props and labels from the old to naya and push with nextcp(use push_helper)
+            if(old['cp_start']==naya['cp_start'] and old['cp_end']==naya['cp_end']): #case when node existed within that cp and vanished then
+                push_with_cp_helper(graph,nextcp,naya)#only node
+            else:
+                naya.properties.clear()
+                naya.labels.clear()
+                for x in old.properties:
+                    naya[x]=old[x]
+                for x in old.labels:
+                    naya.labels.add(x)
+                push_with_cp_helper(graph,nextcp,naya)
+        #increment in both cases
         increment = True
-    relquery = "match ()-[r]->() where r.cp_start>'"+cp+"'  return distinct r.relid"
+        if(naya.bound):
+            naya.unbind()
+        
+    #same logic goes for relations
+    relquery = "match ()-[r]->() where r.cp_start>"+str(cp)+" or (r.cp_end <> "+str(INFINITY)+" and "+str(cp)+"<=r.cp_end) return distinct r.relid"
     relrc = graph.cypher.execute(relquery)
     for r in relrc:
         old = getRelByIdAndCheckPoint(graph,r['r.relid'],cp)
         naya = getRelAtHead(graph,str(r['r.relid'])) ##TODO: change to get headoflinkedlist for dead nodes
-        naya.properties.clear()
-        for x in old.properties:
-            naya[x]=old[x]
-        push_with_cp_helper(graph,nextcp,naya)
+        if old==None:
+            ##existed till the previous checkpoint, from nextcp it is dead
+            if(naya['cp_end']==INFINITY):
+                naya['cp_end']=nextcp-1
+                graph.push(naya)
+        else:
+            if(old['cp_start']==naya['cp_start'] and old['cp_end']==naya['cp_end']):
+                push_with_cp_helper(graph,nextcp,naya)
+            else:
+                naya.properties.clear()
+                for x in old.properties:
+                    naya[x]=old[x]
+                push_with_cp_helper(graph,nextcp,naya)
+        #increment in both cases
+        if(naya.bound):
+            naya.unbind()
         increment = True
     if increment:
         incrementCheckPoint(graph)
+
 
 def main():
     ###
@@ -536,17 +615,23 @@ def main():
     ##or change the db for better
     gt=getGraph('localhost','7474','neo4j','yoyo')
     gt.delete_all()
-    gt.cypher.execute("create (n:version_cp:system {next:'1'}),   (p:node_id:system {next:'1'}),  (q:rel_id:system {next:'1'})")
+    gt.cypher.execute("create (n:version_cp:system {next:1}),   (p:node_id:system {next:1}),  (q:rel_id:system {next:1})")
     test_TwoNodesOneRelation(gt)
-    revert(gt,'3')
+    revert(gt,'4')
+    revert(gt,'8')
+    revert(gt,'7')
+    #test_intLimit(gt)
+    #print getNodeByIdAndCheckPoint(gt,'3','6')
 ##TODO: graph wrapper for new methods
 ##Check django-neo4j and django-reversion
 ##Check neo-kafka
 ##Check method for serailizing and de-serailizing
 
-##Check max what number can go in neo4j
+##Check max what number can go in neo4j - no need for now, as string is working just fine, 
+##we can create indices on cp_start and cp_end
 ##For a particular cp run a match query !!?? TODO? Match on previous nodes shouldnt be there! 
 ##Match always on latest nodes and relation! Never on deleted nodes
 ##TODO: Also check if node deleted, some changes elsewhere, now revert back to state where node existed
 ##Should give new number on reverting??
 main()
+
