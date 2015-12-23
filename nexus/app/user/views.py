5bdcb8f653b080bbd9bc32859fdf085232010b45
abhiagar90 <abhiagar90@gmail.com>
2015-12-23 17:31:05 +0530

    moderate work diff module

--- nexus/app/user/views.py
+++ nexus/app/user/views.py
@@ -4,3 +4,11 @@ from flask import render_template, flash, redirect, session, g, request, url_for
 @user.route('/')
 def show():
     return render_template("temp.html", homeclass="active", temptext='user')
+
+@user.route('/diff')
+def diff():
+	import app.temp as t 
+	graph = t.getGraph()
+	old,naya = t.createNodes(graph)
+	temp = str(old) + '<hr/>' + str(naya)
+	return render_template("temp.html", homeclass="active", temptext=temp)
