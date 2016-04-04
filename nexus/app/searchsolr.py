0e925262197965f5265f1b3a79bfda2f4a424959
Amartya <amartyaamp@gmail.com>
2016-04-05 00:36:41 +0530

    Updated logic searchsolr.py. Working for test data.

--- nexus/app/searchsolr.py
+++ nexus/app/searchsolr.py
@@ -1,35 +1,33 @@
 import requests as rq
 import ast
 
-def get_uuid(propnames=None,propvalues=None,thresvalues=None,label = None):
-    default_url = "http://localhost:8983/solr/mtp/select?q=*%3A*&wt=python&rows=1000&indent=true"
-    if thresvalues == None:
-        thresvalues = ['0.2' for i in range(len(propnames))]
+def get_uuid(propname=None,propvalue=None,thresvalue=None,label = None):
+    default_url = "http://10.237.27.87:8983/solr/mtp/select?q=*%3A*&wt=python&rows=10&indent=true"
+    if thresvalue == None:
+        thresvalue = '0.2'
     if label == None:
         label = '*'
-    if propvalues == None or propnames == None:
+    if propvalue == None or propname == None:
         req_url = default_url
     else:
 
-        base_url = "http://localhost:8983/solr/mtp/select?q="
-        rest_url = "&wt=python&rows=1000&indent=true"
-        propnames_n_values = zip(propnames,propvalues,thresvalues)
+        base_url = "http://10.237.27.87:8983/solr/mtp/select?q="
+        rest_url = "&wt=python&rows=10&indent=true"
 
-        for propname,propvalue,thresval in propnames_n_values:
-            if propname == "name":
-                initials = propval.split(' ')
-                initials = [x+'~'+thresval for x in initials]
-                const_str = '+AND+propvalue%3A'
-                propvalue_str = const_str.join(initials)
-                propvalue_str = const_str[1:]+propvalue_str
-                print "propvalue_str"
+        if propname == "name":
+            initials = propvalue.split(' ')
+            initials = [x+'~'+thresvalue for x in initials]
+            const_str = '+AND+propvalue%3A'
+            propvalue_str = const_str.join(initials)
+            propvalue_str = const_str+propvalue_str
+            print "propvalue_str"
 
-            else:
-                propvalue_str = '+AND+propvalue%3A'+propvalue+'~'+thresval
+        else:
+            propvalue_str = '+AND+propvalue%3A'+propvalue+'~'+thresvalue
 
-            label_str = 'label%3A'+label
-            propname_str = '+AND+propname%3A'+propname
-            arg_url = label_str+propname_str+propvalue_str
+        label_str = 'label%3A'+label
+        propname_str = '+AND+propname%3A'+propname
+        arg_url = label_str+propname_str+propvalue_str
 
         req_url = base_url+arg_url+rest_url
 
@@ -52,5 +50,19 @@ def get_uuid(propnames=None,propvalues=None,thresvalues=None,label = None):
 if __name__=="__main__":
     #l = get_uuid()
     #print l
-    l1 = get_uuid(['candidate'],['navn'])
-    print l1
+    test_lab = 'company'
+    test_propN  = ['name' , 'status']
+    test_propV = ['jindal' , 'active']
+    test_thres = ['0.2','0.3']
+    results = []
+    i = 0
+    for pN,pV,tH in zip(test_propN,test_propV,test_thres):
+        l = get_uuid(propname =pN,propvalue = pV, thresvalue = tH,label = test_lab)
+        print "##Pirnting output "
+        print l
+
+        if i == 0: results = [x for x in l]
+        else: results = [x  for x in results if x in l]
+        i = i+1
+
+    print results
