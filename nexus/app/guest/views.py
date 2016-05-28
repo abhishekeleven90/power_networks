7f6e2dc2b3b3a9a522f4034b8c0ae1ec6e6a364c
Amartya <amartyaamp@gmail.com>
2016-05-28 20:50:36 +0530

    Collect city and state names and some bugfixes in index_entities.

--- nexus/app/guest/views.py
+++ nexus/app/guest/views.py
@@ -13,18 +13,20 @@ def temp2():
     from app.models.dbmodels.index_entities import Entity
     print 'should work fine'
     #Entity.del_all_entities()
-    en = Entity(4,'Kapil','person,politician','Kapil, Thakkar', 'gujrat iit mumbai')
-    rows = en.insertEntity()
-    en.name = 'Amartya'
-    en.updateEntity()
+    #en = Entity(4, 'Kapil', 'person,politician', 'Kapil, Thakkar', 'gujrat iit mumbai')
+    #rows = en.insertEntity()
+    #en.name = 'Amartya'
+    #en.updateEntity()
+    en2= Entity()
+    en2.getEntity(4)
     print 'will be here if all fine'
-    return render_template("temp.html", homeclass="active", temptext = 'You are here')
+    return render_template("temp.html", homeclass="active", temptext ='You are here '+en2.name+' '+en2.labels)
 
 @guest.route('/temp3/')
 def temp3():
     from app.utils.locprocess import getCityState
     (city,state) = getCityState('pondicherry')
-    return render_template("temp.html", homeclass="active", temptext = city+' '+state)
+    return render_template("temp.html", homeclass="active", temptext=city+' '+state)
 
 @guest.route('/viz/')
 def viz():
--- nexus/app/utils/locprocess.py
+++ nexus/app/utils/locprocess.py
@@ -1,23 +1,72 @@
 #process address, location of entities for better resolution
+#Using Google geolocation api
+import googlemaps
+import requests
+import json
+from termcolor import colored
+from googlemaps.exceptions import Timeout, TransportError, ApiError
+from time import sleep
 
-from geolocation.main import GoogleMaps
 
 def getCityState(address):
-    if type(address) !=str:
-        print "Non string argument"
+    if type(address) != str:
+        print colored("Non string argument",'red')
         exit(3)
 
-    gmaps = GoogleMaps(api_key="AIzaSyCPrZzfBD4vpdjkulq2u62CloyZMsAojRQ")
-    loc = gmaps.search(location=address)
-    myloc = loc.first()
-
-    city = myloc.city
-    for area in myloc.administrative_area:
-        if area.name.lower() != city.lower():
-            state = area.name
+    # API KEY OLD AIzaSyALThUkSSrl0qMGPnaewBghOhkA81vDQHk"
+    gmaps = googlemaps.Client(key="AIzaSyAbkl28DtvWs2WCaOUzt7d383my26Invvw")
+    i = 0
+    while (i < 3):
+        try:
+            loc = gmaps.geocode(address)
+        except (TransportError, Timeout, ApiError) as e:
+            print colored("Error - {}".format(e),'red')
+            print colored("Trying to reconnect",'green')
+            sleep(5)
+            i += 1
+        else:
             break
 
-    return (city,state)
+    if i >= 3:
+        print colored("fatal: Network Error!", 'red')
+        exit(1)
+
+    # print loc
+    results = loc[0]["address_components"]
+    print results
+    try:
+        city = [r["long_name"] for r in results if ("administrative_area_level_2" in r["types"])][0]
+    except IndexError:
+        print colored("Not enough info to infer city", 'red')
+        city = ""
+
+    try:
+        state = [r["long_name"] for r in results if "administrative_area_level_1" in r["types"] ][0]
+    except IndexError:
+        print colored("Not enough info to infer state", 'red')
+        state = ""
+
+    return city, state
 
 
+def getCityStateByPin(pincode):
+    url = "https://www.whizapi.com/api/v2/util/ui/in/indian-city-by-postal-code"
+    querystring = {"pin": pincode, "project-app-key": "sd6u15esx571r9kndndlib17"}
+    headers = {
+              'cache-control': "no-cache",
+              'postman-token': "e162eb01-9cb6-6845-0dea-5b24c33e2232"
+              }
 
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = json.loads(response.text)["Data"]
+    city = response[0]["City"]
+    state = response[0]["State"]
+
+    return city, state
+
+
+if __name__ == "__main__":
+    c, s = getCityState('West Bengal Kolkata')
+    #c, s = getCityStateByPin('700065')
+    print c
+    print s
