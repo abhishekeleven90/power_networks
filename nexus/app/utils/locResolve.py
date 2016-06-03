b799c617189bf233c1a6c46c7ef1ac84acb27647
Amartya <amartyaamp@gmail.com>
2016-06-03 22:14:00 +0530

    Location resolving and location error merging

--- nexus/app/utils/locResolve.py
+++ nexus/app/utils/locResolve.py
@@ -2,7 +2,7 @@ import jellyfish as jf
 import pandas as pd
 import requests
 import json
-
+from locprocess import getCityState
 
 def getStates():
     url = "https://www.whizapi.com/api/v2/util/ui/in/indian-states-list"
@@ -28,6 +28,35 @@ def getCities(id):
     resp = json.loads(response.text)
     cities_df = pd.DataFrame(resp['Data'])
     return cities_df
+
+#Given an address, uses getCityState to get city and state and resolve it
+#against google_cities_nodups.csv and google_states.csv
+
+
+def getCityStateResolved(address):
+    dfCities = pd.read_csv("./locdata/google_cities_nodups.csv")
+    dfStates = pd.read_csv("./locdata/google_states.csv")
+    city, state = getCityState(address)
+
+    ## Merge the two dfs
+    df_merged = pd.merge(dfCities, dfStates, left_on='SID', right_on='ID')
+
+    ##Add two new columns for jaro scores in the df
+    df_merged['jaroCity'] = df_merged['city'].map(lambda x: jf.jaro_distance(x.lower(), city.lower()))
+    df_merged['jaroState'] = df_merged['state'].map(lambda x: jf.jaro_distance(x.lower(), state.lower()))
+
+    df_merged['jaroFinal'] = 0.5*df_merged['jaroCity'] + 0.5*df_merged['jaroState']
+    ##Select those rows whose jaro is over a threshold
+    ##df_merged = df_merged[df_merged['jaroFinal'] > 0.9]
+
+    ##Find row with max value for jaro
+    resolvedCityState = df_merged.ix[df_merged['jaroFinal'].idxmax()]
+
+    return resolvedCityState['city'], resolvedCityState['state']
+
+
+
+
 
 if __name__ == "__main__":
     states = getStates()
