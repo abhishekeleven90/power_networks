e740badf90b46fee62bc47c235e8c4b8038a7d39
Amartya <amartyaamp@gmail.com>
2016-06-24 21:53:35 +0530

    generate JSON from LS csv and gitignore JSON files.

--- nexus/app/utils/generateJSON.py
+++ nexus/app/utils/generateJSON.py
@@ -1,5 +1,10 @@
 '''
 This file generates JSON from CSV files
+Key things to make the genrator flexible -
+fields = list of fields to look after to create relationships
+field classes = this is reqd to give respective relationship labels and props
+property names = list of fields to look after as a property for the base user
+data row = data rows can be a json string or a Series in pandas
 '''
 import json
 import pandas as pd
@@ -11,6 +16,7 @@ from termcolor import colored
 id = 0
 
 def getNextID():
+    ''' Indirectly get the next value of id global '''
     global id
     id += 1
     return id
@@ -18,12 +24,13 @@ def getNextID():
 rid = 0
 
 def getNextRID():
+    ''' Indirectly get the next value of rid global (relations) '''
     global rid
     rid +=1
     return rid
 
 def getField(dic, key, by=''):
-
+    ''' Get field according to the key given. Return '' if no key of that name '''
     try:
         v = dic[key]
     except KeyError as e:
@@ -33,17 +40,33 @@ def getField(dic, key, by=''):
         return ''
 
     else:
-        if by == 'date':
-            v = formatFields.getTimeStampLong(v)
-        elif by == 'name':
-            v = formatFields.formatName(v)
+        try:
+            if by == 'date':
+                v = formatFields.getTimeStampLong(v)
+            elif by == 'name':
+                v = formatFields.formatName(v)
 
-        return str(v)
+            return str(v)
+        except UnicodeEncodeError as ud:
+            import traceback
+            print traceback.format_exc()
+            print repr(ud)
+            v = v.encode('ascii', 'ignore')
+            print colored("Encoding error. Returning as -{}".format(str(v)),
+                          'red', attrs=['bold'])
+            return str(v)
 
-def getSelfEntity(JSONstr, sourceurl, labels, fetchdate, i):
+def getDictFromRow(row):
+    ''' Returns a dict from row of the table '''
 
-    d = ast.literal_eval(JSONstr)
+    d = ast.literal_eval(row)
     d = dict((k.strip().lower(), v) for k, v in d.iteritems())
+    return d
+
+def getSelfEntity(row, sourceurl, labels, fetchdate, i):
+    ''' Create the base entity here with all its properties '''
+
+    d = getDictFromRow(row)
     entity = defaultdict()
     entity["id"] = str(getNextID())
     entity["sourceurl"] = sourceurl
@@ -70,14 +93,13 @@ def getSelfEntity(JSONstr, sourceurl, labels, fetchdate, i):
     return entity
 
 def isBidirectional(field):
-    
     bidirectional_fields = ["spouse's name"]
     if field in bidirectional_fields:
         return True
     return False
 
 def getRelLabel(field):
-
+    ''' Generate labels according to classes the field belong to'''
     field_class1 = ["mother's name", "father's name", "spouse's name"]
     #FIXME - constituency label??
     field_class2 = [ "constituency :"]
@@ -89,15 +111,14 @@ def getRelLabel(field):
     else: return "defaultlabel"
         
 def getRelProps(field, independent="False"):
-
-    props = defaultdict()
+    ''' Get relationship properties as per classes of fields '''
     parent_class = ["mother's name", "father's name"]
     spouse_class = ["spouse's name"]
     #FIXME - constituency label?
     institute_class = ["constituency :"]
 
     if field in parent_class:
-        kind = "childof"
+        kind = "ischildof"
         if field == "mother's name":
             subkind = "hasmother"
         else:
@@ -112,7 +133,9 @@ def getRelProps(field, independent="False"):
 
     return dict()
 
-def getRelatedEntities(JSONstr, entity, sourceurl, fetchdate, labels):
+
+def getRelatedEntities(row, entity, sourceurl, fetchdate, labels):
+    ''' According to fields list generate all related entities and their relations '''
 
     # fields list with whom entity has a rel
     #FIXME - maybe the list is inputted as a param??
@@ -120,13 +143,12 @@ def getRelatedEntities(JSONstr, entity, sourceurl, fetchdate, labels):
             , "constituency :"]
 
     baseEntityId = entity["id"]
-    d = ast.literal_eval(JSONstr)
-    d = dict((k.strip().lower(), v) for k, v in d.iteritems())
+    d = getDictFromRow(row)
 
-    #TODO - for all entries in list
+    # - for all entries in list
     e_list, r_list = [], []
     for f in fields_list:
-        #TODO - fill up entity table with props
+        # - fill up entity table with props
         name = getField(d, f, by='name')
         if name == "":
             print colored("No field- {} ".format(f), 'red', attrs=['bold'])
@@ -150,7 +172,7 @@ def getRelatedEntities(JSONstr, entity, sourceurl, fetchdate, labels):
         
         e_list.append(r_entity)
 
-        #TODO - fill relationship table with props
+        # - fill relationship table with props
         relation = defaultdict()
         relation["sourceurl"] = sourceurl
         relation["fetchdate"] = fetchdate
@@ -163,23 +185,24 @@ def getRelatedEntities(JSONstr, entity, sourceurl, fetchdate, labels):
         
         r_list.append(relation)
 
-    #TODO - return entity list and rel list
+    # - return entity list and rel list
     return e_list, r_list
 
 
 def generateJSON(df):
-    #TODO - for each row politician
+    # - for each row politician
     import time
     e_list, r_list = [], []
-    df2 = df[1:2]
+    df2 = df
     for i, r in df2.iterrows():
 
         # create self entity
         properties = r['props']
-        sourceurl, fetchdate = 'www.abc123.com', time.time()
-        self_entity = getSelfEntity(properties, sourceurl,['entity'], str(fetchdate), i)
+        sourceurl = 'http://loksabha.nic.in'
+        fetchdate = formatFields.getTimeStampLong('22 Jun 2016')
+        self_entity = getSelfEntity(properties, sourceurl, ['entity'], str(fetchdate), i)
 
-        #TODO - create list of related entities and relations
+        # - create list of related entities and relations
         related_entities, relations = getRelatedEntities(properties,
                                         self_entity, sourceurl, str(fetchdate), labels=['entity'])
 
@@ -189,20 +212,19 @@ def generateJSON(df):
         r_list += relations
         print colored("Processing {} of {}".format(i,len(df2)), 'red', attrs=['bold'])
 
-    #TODO - wrap up in higher order obj
+    # - wrap up in higher order obj
     #FIXME - use function params for taskid, userid later?
-    result = dict(taskid=1, userid="abhi2@gmail.com", token="token2",
+    result = dict(taskid=4, userid="amartyaamp@gmail.com", token="103a5b7dc528b08d4eeacde292a18b69",
                   entities=e_list, relations=r_list)
 
-    #TODO - convert obj to json and return
+    # - convert obj to json and return
     return json.dumps(result)
     
 if __name__ == "__main__":
     
-    INPUT_DIRECTORY = './'
+    INPUT_DIRECTORY = '/home/amartyac/AlgoPractice/scrapy/L_R_Sabha/'
     df = pd.read_csv(INPUT_DIRECTORY + "L_output.csv")
     data= generateJSON(df)
     obj = open("data.json", "wb")
     obj.write(data)
     obj.close()
-
